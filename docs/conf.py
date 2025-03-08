3edfde6a8e840c233cc05303e75df1622d687841
atomgomba <karcsi@ekezet.com>
2025-03-08 10:47:59 +0100

    Update doc build

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,6 @@ copyright = "The Orangebox Authors"
 version = release = orangebox.__version__
 
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_theme_options = {
     "collapse_navigation": False,
     "sticky_navigation": True,
